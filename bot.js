1684fdf1e02742d41bdd856fbef7ee497c73fa2f
TBAPuHKA <50981620+TBAPuHKA@users.noreply.github.com>
2020-01-31 16:01:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,18 +15,19 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-  if (message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix)) {
 
-  if (message.content.startsWith(prefix + 'test')) {
+   if (message.content.startsWith(prefix + 'test')) {
     message.channel.send('```we invading the world, comrade!``` :duck:');
+   }
   }
-
+/*
   if (message.content ==='atma') {
 
     message.channel.send(':regional_indicator_s: :regional_indicator_h: :regional_indicator_a: :regional_indicator_m: :regional_indicator_e:');
 
  }
-
+*/
 /*
   if (message.content === 'atma') {
 
