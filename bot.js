ee203b1fb86b1b1180ad841261d153f812b4e530
TBAPuHKA <50981620+TBAPuHKA@users.noreply.github.com>
2020-01-31 17:21:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const prefix = "atma.";
+const prefix = "at.";
 
  
 /*
@@ -18,14 +18,14 @@ client.on('message', message => {
 
   if (message.content.startsWith(prefix)) {
 
-    if (message.content ===prefix) {
+    if (message.content ===prefix+'ma') {
       message.channel.send(':regional_indicator_s: :regional_indicator_h: :regional_indicator_a: :regional_indicator_m: :regional_indicator_e:');
     } 
 
     if (message.content.startsWith(prefix + 'test')) {
      message.channel.send('```we invading the world, comrade!``` :duck:');
     }
-    
+
    else { message.channel.send('wrong command, comrade :duck:')
    }
   }
