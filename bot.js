4dfb0942ca47d2cb695f7d2e1585b9b2848c6343
TBAPuHKA <50981620+TBAPuHKA@users.noreply.github.com>
2020-01-31 14:52:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('message', message => {
   //if (message.content.startsWith(prefix)) return;
 
   if (message.content.startsWith(prefix + 'test')) {
-    message.channel.send('```check```');
+    message.channel.send('```we invading the world!``` :duck:');
   }
 
   if (message.content ==='atma') {
